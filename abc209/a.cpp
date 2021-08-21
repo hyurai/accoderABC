bb04628efbf1efde9e2b2fbb30b9e4c86f367c18
hyurai <yuumanagai0504@icloud.com>
2021-08-21 14:06:48 +0900

    ac

--- abc209/a.cpp
+++ abc209/a.cpp
@@ -2,24 +2,44 @@
 using namespace std;
  
 int main(){
-    int N,X;
-    cin >> N >> X;
-    vector<int> A(N+1);
-    for(int i =1;i<=N;i++){
-        cin >> A.at(i);
+    int H,W;
+    cin >> H >> W;
+    vector<vector<char>> vec(H+2,vector<char>(W+2));
+    for(int i=1;i<=H;i++){
+        for(int j=1;j<=W;j++){
+            cin >> vec[i][j];
+        }
     }
-    int sum =0;
-    for(int i=1;i<=N;i++){
-        if( i%2 ==0 || i==0){
-            sum = sum + A.at(i) -1 ;
-        }else{
-            sum = sum + A.at(i);
+    for(int i=0;i<H+2;i++){
+        for(int j =0;j<1;j++){
+            vec[i][j] = '#'; 
+        }
+    }
+    for(int i=0;i<1;i++){
+        for(int j =0;j< W+2;j++){
+            vec[i][j] = '#';
+        }
+    }
+    for(int i = 0;i<H+2;i++){
+        for(int j=W+1;j< W+2;j++){
+            vec[i][j] = '#';
+        }
+    }
+    for(int i=H+1;i<H+2;i++){
+        for(int j=0;j<W+2;j++){
+            vec[i][j] = '#';
         }
     }
 
-    if(X >= sum){
-        cout << "Yes" << endl;
-    }else{
-        cout << "No" << endl; 
+    for(int i=0;i<H+2;i++){
+        for(int j=0;j<W+2;j++){
+            if(j== W+1){
+                cout << vec[i][j] << endl;
+            }else{
+                cout << vec[i][j];
+            }
+           
+        }
     }
+
 }
